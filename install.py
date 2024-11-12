350f330a0fed8b612bfd6965cbb0693d69338f2b
Cavus Mustafa <mustafa.cavus@intel.com>
2024-11-12 14:59:47 -0800

    Install req path fix

--- install.py
+++ install.py
@@ -12,7 +12,6 @@ default_command_live = (os.environ.get('WEBUI_LAUNCH_LIVE_OUTPUT') == "1")
 
 def install():
     # install_requirements()
-    req = "requirements.txt"
     launch.run(f"pip install -r {req}", f"Install requirements for OpenVINO",
                f"Error: OpenVINO dependencies were not installed correctly, OpenVINO extension will not be enabled", live=default_command_live)
 
